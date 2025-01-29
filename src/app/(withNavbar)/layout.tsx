df591a2f939df347cb68914b40ca795583d1be16
carrotbat410 <carrotbat410@gmail.com>
2025-01-29 15:44:22 +0900

    컴포넌트폴더만 대문자로 시작하기

--- src/app/(withNavbar)/layout.tsx
+++ src/app/(withNavbar)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-import NavigationBar from "../components/navigationBar/NavigationBar"
+import NavigationBar from "../components/NavigationBar/NavigationBar"
 
 export default function Layout({
     children,
