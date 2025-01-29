6201e093ba95ac7c4ef95bb99be10f18a7f00bd6
carrotbat410 <carrotbat410@gmail.com>
2025-01-29 15:46:31 +0900

    login route 폴더 생성

---
+++ src/app/(withNavbar)/login/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>로그인 페이지</div>
+      </main>
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
+}
