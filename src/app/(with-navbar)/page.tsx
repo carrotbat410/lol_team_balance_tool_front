f43bc58e47df9fcd1322970dde0a5e6c78d0fabf
carrotbat410 <carrotbat410@gmail.com>
2025-01-29 15:18:56 +0900

    group route(with-navbar) 적용

--- src/app/(with-navbar)/page.tsx
+++ src/app/(with-navbar)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <div>인덱스 페이지</div>
       </main>
       <footer className={styles.footer}>
       </footer>
