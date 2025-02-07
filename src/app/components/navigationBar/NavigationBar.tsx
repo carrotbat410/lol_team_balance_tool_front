c451b49f19020019ed2b48eb46605c023a39fb7c
carrotbat410 <carrotbat410@gmail.com>
2025-02-07 19:26:23 +0900

    메뉴 목록 추가

--- src/app/components/navigationBar/NavigationBar.tsx
+++ src/app/components/navigationBar/NavigationBar.tsx
@@ -5,6 +5,8 @@ const menuItems = [
   { label: "로고", href: "/" },
   { label: "커뮤니티", href: "/freeBoard" },
   { label: "팀짜기", href: "/teamBalance" },
+  { label: "문의하기", href: "/tmp1" },
+  { label: "로그인", href: "/login" },
 ];
 
 export default function NavigationBar() {
