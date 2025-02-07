c08395c8724a5be1788b66bbc4d55d226d38b1a8
carrotbat410 <carrotbat410@gmail.com>
2025-02-07 19:08:25 +0900

    팀짜기 페이지 라우트 추가

---
+++ src/app/(with-navbar)/teamBalance/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div>팀짜기 페이지</div>
+      </main>
+    </div>
+  );
+}
