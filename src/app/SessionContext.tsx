461525f403750a76ee5b2b331d8292c7c8e85317
carrotbat410 <carrotbat410@gmail.com>
2025-03-26 16:49:16 +0900

    세션 요청 따로 할필요 없을거같아 일단 주석처리함

--- src/app/SessionContext.tsx
+++ src/app/SessionContext.tsx
@@ -19,14 +19,26 @@ const SessionContext = createContext<SessionContextType | null>(null);
 export function SessionProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/session", {
-      credentials: "include", //모든 요청에 인증 정보를 포함 (e.g. 세션 쿠키)
-    })
-      .then((res) => res.json())
-      .then((data) => setUser(data.user))
-      .catch(() => setUser(null));
-  }, []);
+  // useEffect(() => {
+  //   console.log("여기??")
+  //   fetch("http://localhost:8080/session", {
+  //     credentials: "include", //모든 요청에 인증 정보를 포함 (e.g. 세션 쿠키)
+  //   })
+  //     .then((res) => {
+  //       console.log("Response object:", res);
+
+  //       return res.json()
+  //     })
+  //     .then((data) => {
+  //       console.log("data:", data)
+  //       return setUser(data.user)
+  //     })
+  //     .catch(() => 
+  //     {
+  //       console.log("error:?")
+  //       setUser(null)
+  // });
+  // }, []);
 
   return <SessionContext.Provider value={{ user, setUser }}>{children}</SessionContext.Provider>;
 }
@@ -34,6 +46,7 @@ export function SessionProvider({ children }: { children: React.ReactNode }) {
 // useSession()의 타입을 명확하게 정의
 export function useSession(): SessionContextType {
   const context = useContext(SessionContext);
+  console.log("context", context)
   if (!context) {
     throw new Error("useSession must be used within a SessionProvider");
   }
